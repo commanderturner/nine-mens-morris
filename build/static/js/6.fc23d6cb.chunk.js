b6e051979960c4583208710b65758ce058a660d2
Mark Turner <m.turner@zengenti.com>
2019-09-07 10:08:02 +0100

    build

---
+++ build/static/js/6.fc23d6cb.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonpnine-mens-morris"]=window["webpackJsonpnine-mens-morris"]||[]).push([[6],{144:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",function(){return i});var o=e(13),i=(e(1),function(){var n=window;n.addEventListener("statusTap",function(){Object(o.g)(function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then(function(){Object(o.n)(function(){return r.scrollToTop(300)})})}})})})}}]);
+//# sourceMappingURL=6.fc23d6cb.chunk.js.map
